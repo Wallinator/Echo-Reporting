720eb53fa72ed0ec8be973a14b3229cf42368e78
Wallinator <40655366+Wallinator@users.noreply.github.com>
2020-09-03 14:42:53 +1000

    initial commit

---
+++ DicomFileReading/DicomManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Dicom;
+
+namespace DICOMReporting.DicomFileReading {
+	public class DicomManager {
+		public static void test() {
+			DicomFile file = DicomFile.Open("../../../test materials/STUDYX0W");
+
+			var walker = new DicomDatasetWalker(file.Dataset);
+
+			//GetFiles();
+			walker.Walk(new Walker());
+
+			/*var en = file.Dataset.GetEnumerator();
+			while (en.MoveNext()) {
+				if (!en.Current.ValueRepresentation.IsMultiValue) {
+					System.Diagnostics.Debug.WriteLine(en.Current.Tag.DictionaryEntry.Name);
+				}
+				//System.Diagnostics.Debug.WriteLine(en.Current.ValueRepresentation.IsMultiValue);
+			}*/
+		}
+		public static void GetFiles() {
+			string directory = "../../../test materials";
+			var dirinfo = new DirectoryInfo(directory);
+			List<DicomFile> filenames = new List<DicomFile>();
+			var fileenum = dirinfo.EnumerateFiles("*", new EnumerationOptions() { RecurseSubdirectories = true, IgnoreInaccessible = true }).GetEnumerator();
+			while (fileenum.MoveNext()) {
+				DicomFile file;
+				try {
+					file = DicomFile.Open(fileenum.Current.FullName);
+				}
+				catch (Exception) {
+					continue;
+				}
+				if (file.Dataset.InternalTransferSyntax.Equals(DicomTransferSyntax.ImplicitVRLittleEndian)) {
+					filenames.Add(file);
+					System.Diagnostics.Debug.WriteLine(fileenum.Current.Name);
+				}
+			}
+		}
+	}
+}
---
+++ DicomFileReading/Walker.cs
@@ -0,0 +1,84 @@
+﻿using CharLS;
+using Dicom;
+using Dicom.IO.Buffer;
+using Dicom.StructuredReport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DICOMReporting.DicomFileReading {
+	public class Walker : IDicomDatasetWalker {
+		//public string level = "";
+		public bool OnBeginFragment(DicomFragmentSequence fragment) {
+			//System.Diagnostics.Debug.WriteLine(fragment.Tag.DictionaryEntry.Name);
+			return true;		
+		}
+
+		public bool OnBeginSequence(DicomSequence sequence) {
+
+			DicomDataset measuredvaluesequence;
+			if (sequence.Tag.Equals(DicomTag.ContentSequence)) {
+				DicomCodeItem codeitem;
+				try {
+					codeitem = sequence.Items.First().GetCodeItem(DicomTag.ConceptNameCodeSequence);
+				}
+				catch (Exception) {
+					return true;
+				}
+				if (codeitem.Value == "121033") {
+					measuredvaluesequence = sequence.Items.First().GetMeasuredValue(DicomTag.MeasuredValueSequence);
+					System.Diagnostics.Debug.WriteLine(codeitem.Value + "  " + codeitem.Meaning + "  " + measuredvaluesequence.GetString(DicomTag.NumericValue));
+				}
+			}
+			return true;
+		}
+
+		public bool OnBeginSequenceItem(DicomDataset dataset) {
+			//level += " ";
+			return true;
+		}
+
+		public void OnBeginWalk() {
+
+		}
+
+		public bool OnElement(DicomElement element) {
+			/*
+						if (element.ValueRepresentation.IsString) {
+							System.Diagnostics.Debug.WriteLine(element.Get<string>());
+						}*/
+			return true;
+		}
+
+		public Task<bool> OnElementAsync(DicomElement element) {
+			return Task.Run(() => { return true; });
+		}
+
+		public bool OnEndFragment() {
+			return true;
+		}
+
+		public bool OnEndSequence() {
+			//level.Remove(level.Length - 1, 1);
+			return true;
+		}
+
+		public bool OnEndSequenceItem() {
+			//level.Remove(level.Length - 1, 1);
+			return true;
+		}
+
+		public void OnEndWalk() {
+		}
+
+		public bool OnFragmentItem(IByteBuffer item) {
+			return true;
+		}
+
+		public Task<bool> OnFragmentItemAsync(IByteBuffer item) {
+			return Task.Run(() => { return true; });
+		}
+	}
+}
